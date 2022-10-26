b25435f2dfc691411ab417b67494dfc55c38cd7b
Mr Reid <64812253+sreid111@users.noreply.github.com>
2022-10-27 09:51:54 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+function purple () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+    strip.show()
+}
+input.onButtonPressed(Button.A, function () {
+    flag += 1
+})
+function rainbow () {
+    strip.showRainbow(1, 360)
+    strip.show()
+    for (let index = 0; index <= 13; index++) {
+        strip.rotate(1)
+        strip.show()
+        basic.pause(100)
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    flag = 0
+})
+function police () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+    basic.pause(200)
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    strip.show()
+    basic.pause(200)
+}
+let flag = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 13, NeoPixelMode.RGB)
+flag = 0
+basic.forever(function () {
+    if (flag == 0) {
+        purple()
+    }
+    if (flag == 1) {
+        rainbow()
+    }
+    if (flag == 2) {
+        police()
+    }
+    if (flag > 3) {
+        flag = 0
+    }
+})
