6394eb1b76a21b6274efd9a730638d96c0e6ef4b
Mr Reid <64812253+sreid111@users.noreply.github.com>
2022-11-21 07:53:46 +1000

    Update police to flash side to side

--- main.ts
+++ main.ts
@@ -14,9 +14,6 @@ function rainbow () {
         basic.pause(100)
     }
 }
-input.onButtonPressed(Button.AB, function () {
-	
-})
 input.onButtonPressed(Button.B, function () {
     flag = 0
 })
@@ -32,12 +29,25 @@ function red () {
     basic.pause(500)
 }
 function police () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    strip.clear()
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Red))
     strip.show()
-    basic.pause(200)
-    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.pause(500)
+    strip.clear()
+    strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(12, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(13, neopixel.colors(NeoPixelColors.Blue))
     strip.show()
-    basic.pause(200)
+    basic.pause(500)
 }
 let flag = 0
 let strip: neopixel.Strip = null
@@ -52,13 +62,13 @@ basic.showLeds(`
     `)
 basic.forever(function () {
     if (flag == 0) {
-        purple()
-    }
-    if (flag == 1) {
         rainbow()
     }
-    if (flag == 2) {
+    if (flag == 1) {
         police()
+    }
+    if (flag == 2) {
+        purple()
     }
     if (flag == 3) {
         red()
